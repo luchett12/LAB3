38b3df55b2b73958fa9c00caefe7449f1874cbf8
Lucas Bustos <82196657+luchett12@users.noreply.github.com>
2021-06-04 17:22:38 -0300

    Delete GuiaEjerciciosN1 directory

--- GuiaEjerciciosN1/Eje13.py
+++
@@ -1,6 +0,0 @@
-email = input("INGRESE SU EMAIL: ")
-
-if "@" in email:
-        print("SU EMAIL ES VALIDO.")
-else:
-        print("SU EMAIL NO ES VALIDO.ff")
--- GuiaEjerciciosN1/Eje14.py
+++
@@ -1,9 +0,0 @@
-nombre = "mAriA"
-
-nombre_mayusculas = nombre.upper()
-nombre_minusuclas = nombre.lower()
-nombre_cap = nombre.capitalize()
-
-print(f'Mayusculas: {nombre.upper()}, \nMinusculas: {nombre.lower()}, \nPrimera letra: {nombre.capitalize()}')
-
-
